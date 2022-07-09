9d6518c2e82c10f8ab9bb6ca423b32d0a841140f
Tom Gregor <tgregor@ivsoftware.com>
2022-07-09 12:04:50 -0600

    Add project files.

---
+++ paint_class/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace paint_class
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            _paint.Refresh += (sender, e) =>
+            {
+                // Causes the control to repaint.
+                Refresh();
+                Text = CurrentTestColor.ToString();
+            };
+
+            // Draw diagonal line adding 25 to offset each time.
+            buttonDiag.Click += (sender, e) =>
+                _paint.DrawDiagonal(GetNextTestColor(), offsetX: 25 * _testCountDiag++);
+
+            buttonLine.Click += (sender, e) =>
+            {
+                var offsetY = 25 * _testCountLine++;
+                _paint.Drawline(
+                    GetNextTestColor(),
+                    new PointF(0, 100 + offsetY),
+                    new PointF(ClientRectangle.Width, 100 + offsetY));
+            };
+
+            buttonClear.Click += (sender, e) =>
+            {
+                _paint.Clear();
+                _testCountDiag = _testCountLine = 0;
+            };
+        }
+        int _testCountDiag = 0;
+        int _testCountLine = 0;
+        PaintClass _paint = new PaintClass();
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            _paint.PaintAll(e.Graphics, always: _always);
+            _always = false;
+        }
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            if(!_always)
+            {
+                _always = true;
+                Refresh();
+            }
+        }
+        bool _always = false;
+
+        internal Color GetNextTestColor()
+        {
+            CurrentTestColor = _knownColors[_randomColorForTest.Next(_knownColors.Length)];
+            return CurrentTestColor; // For convenience
+        }
+
+        // T E S T I N G
+        Color[] _knownColors { get; } =
+                Enum.GetValues(typeof(KnownColor))
+                .Cast<KnownColor>()
+                .Select(known => Color.FromKnownColor(known))
+                .ToArray();
+        public Color CurrentTestColor { get; private set; }
+        Random _randomColorForTest = new Random();
+    }
+}
---
+++ paint_class/PaintClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace paint_class
+{
+    class PaintClass : List<PaintClassContext>
+    {
+        public new void Add(PaintClassContext context)
+        {
+            base.Add(context);
+            _modified = true;
+            Refresh?.Invoke(this, EventArgs.Empty);
+        }
+        public event EventHandler Refresh;
+        private bool _modified = false;
+        public void Drawline(Color color, PointF start, PointF end) =>
+            Add(new PaintClassContext
+            {
+                PaintOp = PaintOp.DrawLine,
+                Color = color,
+                Start = start,
+                End = end,
+            });
+
+        public new void Clear()
+        {
+            base.Clear();
+            Add(new PaintClassContext
+            {
+                PaintOp = PaintOp.Clear,
+            });
+        }
+        public void DrawDiagonal(Color color, float offsetX = 0, float offsetY = 0) =>
+            Add(new PaintClassContext
+            {
+                PaintOp = PaintOp.DrawDiagonal,
+                Color = color,
+                OffsetX = offsetX,
+                OffsetY = offsetY,
+            });
+
+        private void paint(Graphics graphics, PaintClassContext context)
+        {
+            switch (context.PaintOp)
+            {
+                case PaintOp.DrawDiagonal:
+                    using (var pen = new Pen(context.Color, 2f))
+                    {
+                        var rect = graphics.VisibleClipBounds;
+                        var diagonalStart = new PointF(
+                                rect.X + context.OffsetX,
+                                rect.Y + context.OffsetY);
+                        var diagonalEnd = new PointF(
+                                rect.X + rect.Width + context.OffsetX,
+                                rect.Y + rect.Height + context.OffsetY);
+                        graphics.DrawLine(pen, diagonalStart, diagonalEnd);
+                    }
+                    break;
+                case PaintOp.DrawLine:
+                    using (var pen = new Pen(context.Color, 2f))
+                    {
+                        graphics.DrawLine(pen, context.Start, context.End);
+                    }
+                    break;
+                case PaintOp.Clear:
+                    using (var brush = new SolidBrush(Color.Transparent))
+                    {
+                        graphics.FillRectangle(brush, graphics.VisibleClipBounds);
+                    }
+                    base.Clear(); // Remove the clear action
+                    break;
+                default:
+                    throw new NotImplementedException("ToDo");
+            }
+        }
+
+        internal void PaintAll(Graphics graphics, bool always = false)
+        {
+            if (always || _modified)
+            {
+                foreach (var context in this.ToArray())
+                {
+                    paint(graphics, context);
+                }
+            }
+            _modified = false;
+        }
+    }
+    enum PaintOp{ DrawLine, Clear, DrawDiagonal}
+    class PaintClassContext
+    {
+        public PaintOp PaintOp { get; set; }
+        public Color Color { get; set; }
+        public PointF Start { get; set; }
+        public PointF End { get; set; }
+        public float OffsetX { get; set; }
+        public float OffsetY { get; set; }
+    }
+}
